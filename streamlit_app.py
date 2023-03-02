1fb9cc04df509a9bae26fa75592c14c01f3cc07f
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-03-02 14:28:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Read CSV file from S3 bucket
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# Choose the Fruit Name Column as the Index
+
+# Choose the Fruit name column as the Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+# Display the table on the app page
+streamlit.dataframe(my_fruit_list)
+
 # Filter the table data by selecting the fruits
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page 
-streamlit.dataframe(fruits_to_show)
